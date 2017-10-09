e6f13db4453211b67ea9baccee49932f5d01b625
Tatuev Anton <tatuev.a.a@yandex.ru>
2017-10-09 18:20:00 +0300

    Добавил комментарии

--- ball.h
+++ ball.h
@@ -10,20 +10,39 @@ class Ball : public QGraphicsItem
 public:
     Ball(QPointF pos, QPointF V = QPointF(0, 0), QGraphicsItem *parent = 0);
 
+    /**
+     * @brief Установить состояние шарика
+     * @param[in] pos новые координаты шарика
+     * @param[in] V новая скорость шарика
+     */
     void setState(QPointF pos, QPointF V = QPointF(0, 0));
+
+    /**
+     * @brief Получить координаты шарика
+     * @return координаты шарика (x, y)
+     */
     QPointF getPos();
+
+    /**
+     * @brief Получить скорость шарика
+     * @return скорость шарика (Vx, Vy)
+     */
     QPointF getV();
+
+    /**
+     * @brief Получить радиус шарика
+     * @return радиус шарика в пикселях
+     */
     qreal getRadius();
 
 protected:
     void advance(int step) override;
+
 private:
     QPointF xy;
     QPointF V;
     qreal radius;
-
     std::mutex stateMutex;
-
     QColor color;
 
     QPainterPath shape() const;
